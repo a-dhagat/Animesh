ef6e00f8b174b46b79da7ad08251e9f06f86bdb6
a-dhagat <adhagat@andrew.cmu.edu>
2019-11-27 00:51:35 -0500

    bash-changes

---
+++ git-push.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+git config --global user.email "adhagat@andrew.cmu.edu"
+git config --global user.name "a-dhagat"
+echo "Pushing ..."
+
+git add .
+
+git commit -m $1
+
+git push
+
+echo "Pushed Successfully!"
